6aad2e38bd4157d33b6c0bc7038e4726190b4738
Pjotrulo <piotrgdwn@gmail.com>
2022-08-20 12:02:19 +0200

    Added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './components/StartPage';
+import './App.scss';
+import StartPage from "./components/StartPage";
+
+function App() {
+  return (
+    <>
+      <StartPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/StartPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StartPage = () => {
+    return (
+        <>
+            <h1>Hello, World!</h1>
+        </>
+    )
+}
+
+export default StartPage;
